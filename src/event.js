2b4145b1d98412a4d9580959481217e86eb9ad74
vitalets <vitalets@yandex-team.ru>
2014-10-01 18:08:49 +0400

    event: remove sinon stubs

--- src/event.js
+++ src/event.js
@@ -1,62 +1,60 @@
 var sinon = require('sinon');
 var sandbox = sinon.sandbox.create();
 
+function assertFunction(func) {
+    if (typeof func !== "function") {
+        throw new Error("Type error. Passed argument is not a function");
+    }
+}
+
 var EventEmitter = function () {
     this._events = [];
 
     this.addListener = function (handler) {
-        this._assertFunction(handler);
-
-        var index = this._events.indexOf(handler);
-        if (index >= 0) {
+        assertFunction(handler);
+        if (!this.hasListener(handler)) {
             this._events.push(handler);
         }
     };
 
     this.removeListener = function (handler) {
         var index = this._events.indexOf(handler);
-
         if (index >= 0) {
             this._events.splice(index, 1);
         }
     };
 
     this.hasListener = function (handler) {
-        this._assertFunction(handler);
         return this._events.indexOf(handler) >= 0;
     };
-
-    this.addListener = sandbox.spy(this, 'addListener');
-    this.removeListener = sandbox.spy(this, 'removeListener');
-    this.hasListener = sandbox.spy(this, 'hasListener');
 };
 
 EventEmitter.prototype = {
-
-    willTrigger: function () {
-        return this._events.length > 0;
+    /**
+     * Similar to sinon's yields: store data to pass to handler when event triggered
+     */
+    yields: function() {
+        this.data = new Array(arguments.length);
+        for (var i = 0; i < arguments.length; i++) {
+            this.data[i] = arguments[i];
+        }
     },
 
-    _trigger: function () {
-        var args = [].slice.call(arguments);
+    trigger: function () {
+        var args = arguments.length ? [].slice.call(arguments) : this.data;
         var self = this;
-        this._events.forEach(function (handler) {
-            self._assertFunction(handler);
-            handler.apply(null, args);
-        })
+        process.nextTick(function() {
+            self._events.forEach(function (handler) {
+                assertFunction(handler);
+                handler.apply(null, args);
+            })
+        });
     },
 
     _reset: function () {
         this._events = [];
         sandbox.reset();
-    },
-
-    _assertFunction: function (func) {
-        if (typeof func !== "function") {
-            throw new Error("Type error. Passed argument is not a function");
-        }
     }
 };
-
 
 module.exports = EventEmitter;

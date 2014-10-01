8d2bb56578cc3644d8211fe1719225097adf51f9
vitalets <vitalets@yandex-team.ru>
2014-10-01 18:09:10 +0400

    chrome: refactor for lazy loading

--- src/chrome.js
+++ src/chrome.js
@@ -1,28 +1,119 @@
+var fs = require('fs');
+var path = require('path');
 var sinon = require('sinon');
+var EventEmitter = require('./event');
 var sandbox = sinon.sandbox.create();
 
-var EventEmitter = require('./event');
+// var accessor = function () {
+//     return {
+//         get: sandbox.stub(),
+//         set: sandbox.stub()
+//     }
+// };
 
-var accessor = function () {
-    return {
-        get: sandbox.stub(),
-        set: sandbox.stub()
+// var getEmitter = function () {
+//     return new EventEmitter();
+// };
+
+var NO_ARGS = 0;
+var ONE_ARG = 1;
+var MANY_ARGS = 2;
+var NO_CALLBACK = 3;
+
+
+var cache = {};
+
+/**
+ * Lazy getter for methods
+ */
+function getter(prop, methods) {
+    if (cache[prop]) {
+        return cache[prop];
+    }
+    cache[prop] = {};
+    Object.keys(methods).forEach(function(k) {
+        if (k.substring(0, 2) === 'on') {
+            // event
+            var emitter = new EventEmitter();
+            emitter.addListener = sandbox.spy(emitter, 'addListener');
+            emitter.removeListener = sandbox.spy(emitter, 'removeListener');
+            emitter.hasListener = sandbox.spy(emitter, 'hasListener');
+            cache[prop][k] = emitter;
+        } else {
+            // method
+            cache[prop][k] = sandbox.stub();
+        }
+
+        if (methods[k] !== NO_CALLBACK) {
+            // data passed to callback
+            var data;
+            if (methods[k] === NO_ARGS) {
+                data = [];
+            } else {
+                var filename = path.resolve(__dirname, path.join('data', prop, k + '.json'));
+                if (fs.existsSync(filename)) {
+                    // read data from file
+                    data = require(filename);
+                    if (methods[k] === ONE_ARG) {
+                        data = [data];
+                    }
+                    if (k === 'onMessage') {
+                        data.push(sandbox.spy());
+                    }
+                }
+            }
+            cache[prop][k].yields.apply(cache[prop][k], data);
+        }
+    });
+    return cache[prop];
+}
+
+/**
+ * chrome.* APi mocks
+ */
+var chrome = {
+    get _sandbox() {
+        return sandbox;
+    },
+    get tabs() {
+        return getter('tabs',  {
+            get: ONE_ARG,
+            query: ONE_ARG,
+            onUpdated: MANY_ARGS,
+            onRemoved: MANY_ARGS,
+            onReplaced: MANY_ARGS,
+        });
+    },
+    get runtime() {
+        return getter('runtime',  {
+            onMessage: MANY_ARGS
+        });
+    },
+    get windows() {
+        return getter('windows',  {
+            getAll: ONE_ARG
+        });
+    },
+    get browserAction() {
+        return getter('browserAction',  {
+            setIcon: NO_ARGS,
+            setTitle: NO_CALLBACK,
+            setBadgeText: NO_CALLBACK,
+            setBadgeBackgroundColor: NO_CALLBACK,
+            getTitle: MANY_ARGS
+        });
+    },
+    get webRequest() {
+        return getter('webRequest',  {
+            onBeforeRequest: NO_ARGS,
+            onCompleted: NO_ARGS,
+            onErrorOccurred: NO_ARGS
+        });
     }
 };
 
-var getEmitter = function () {
-    return new EventEmitter();
-};
-
-module.exports = {
-    browserAction: {
-        onClicked: getEmitter(),
-        setIcon: sandbox.stub(),
-        setTitle: sandbox.stub(),
-        setBadgeText: sandbox.stub(),
-        setBadgeBackgroundColor: sandbox.stub(),
-        setPopup: sandbox.stub()
-    },
+module.exports = chrome;
+/*
     runtime: {
         onMessage: getEmitter(),
         onMessageExternal: getEmitter(),
@@ -30,7 +121,7 @@ module.exports = {
         sendMessage: sandbox.stub(),
         getManifest: sandbox.stub(),
         setUninstallUrl: sandbox.stub(),
-        id: "12345678910111213141516"
+        //id: "12345678910111213141516"
     },
     extension: {
         getBackgroundPage: sandbox.stub(),
@@ -45,18 +136,18 @@ module.exports = {
         onEnabled: getEmitter(),
         onDisabled:getEmitter()
     },
-    tabs: {
-        update: sandbox.stub(),
-        create: sandbox.stub(),
-        get: sandbox.stub(),
-        reload: sandbox.stub(),
-        sendMessage: sandbox.stub(),
-        remove: sandbox.stub(),
-        getCurrent: sandbox.stub(),
-        onActivated: getEmitter(),
-        onUpdated: getEmitter(),
-        onCreated: getEmitter()
-    },
+    // tabs: {
+    //     update: sandbox.stub(),
+    //     create: sandbox.stub(),
+    //     get: sandbox.stub(),
+    //     reload: sandbox.stub(),
+    //     sendMessage: sandbox.stub(),
+    //     remove: sandbox.stub(),
+    //     getCurrent: sandbox.stub(),
+    //     onActivated: getEmitter(),
+    //     onUpdated: getEmitter(),
+    //     onCreated: getEmitter()
+    // },
     webRequest: {
         onBeforeRequest: getEmitter(),
         onCompleted: getEmitter(),
@@ -70,7 +161,6 @@ module.exports = {
         get: sandbox.stub(),
         getAll: sandbox.stub(),
         set: sandbox.stub(),
-        setAll: sandbox.stub(),
         remove: sandbox.stub(),
         getAllCookieStores: sandbox.stub(),
         onChanged: EventEmitter()
@@ -101,4 +191,5 @@ module.exports = {
         onCreated: getEmitter(),
         getCurrent: sandbox.stub()
     }
-};
+
+};*/

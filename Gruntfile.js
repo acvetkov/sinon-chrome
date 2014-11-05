464c4b9e1de840381b25be8733e70ae0e7527394
vitalets <vitalets@yandex-team.ru>
2014-11-05 17:25:56 +0300

    remove jscs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,11 @@
 module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-jscs');
 
 	grunt.config('jshint', {
 		all: ['Gruntfile.js', 'src/*.js', 'example/src/*.js', 'example/test/*.js']
 	});
 
 	grunt.registerTask('check', [
-		'jshint',
+		'jshint'
 	]);
 };

053f7da127a81515e45790895139dcddd937d8af
vitalets <vitalets@yandex-team.ru>
2014-11-18 13:13:41 +0300

    example: add about black filter

--- example/test/beforeeach.js
+++ example/test/beforeeach.js
@@ -26,6 +26,14 @@ beforeEach(function() {
   };
 
   page.onInitialized = function() {
+    // exclude `about:blank`
+    var isBlank = page.evaluate(function() {
+        return window.location.href === 'about:blank';
+    });
+    if (isBlank) {
+        return;
+    }
+
     page.injectJs(node_modules + 'chai/chai.js');
     page.injectJs(node_modules + 'sinon/pkg/sinon-1.11.1.js');
     page.injectJs(node_modules + 'sinon-chrome/chrome.js');

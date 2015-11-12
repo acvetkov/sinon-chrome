7f15552bad1963953afd6cfbe118d9aa4fd56217
Aleksey Tsvetkov <acvetkov@yandex-team.ru>
2015-11-12 23:31:39 +0300

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/**
+ * @author acvetkov@yandex-team.ru
+ * @overview
+ */
+
+'use strict';
+
+var packageJson = require('./package.json');
+var path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, './src/index.js'),
+    output: {
+        path: path.resolve(__dirname, './out/build/'),
+        filename: 'sinon-chrome.' + packageJson.version + '.js',
+        library: 'chrome',
+        libraryTarget: 'umd'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};

e8046b19dfad854dee3f05bf82dc8c1be2eb61a1
Aleksey Tsvetkov <acvetkov@yandex-team.ru>
2015-12-31 16:57:37 +0300

    add cookie module

---
+++ src/plugins/cookies/cookie.js
@@ -0,0 +1,43 @@
+/**
+ * @author acvetkov@yandex-team.ru
+ * @overview Chrome cookie emulation class
+ */
+
+import URI from 'URIjs';
+
+export default class ChromeCookie {
+
+    constructor (details) {
+        ChromeCookie.assertParams(details);
+        this.details = details;
+    }
+
+    toString () {
+        const domain = this.details.url.replace(/^https?:\/\//i, '');
+        const data = {
+            url: this.details.url,
+            name: this.details.name || '',
+            value: this.details.value || '',
+            domain: domain,
+            hostOnly: domain.charAt(0) === '.',
+            httpOnly: Boolean(this.details.httpOnly),
+            secure: Boolean(this.details.secure),
+            session: !Boolean(this.details.expirationDate),
+            path: this.details.path || (new URI(this.details.url)).path()
+        };
+        if (this.details.expirationDate) {
+            data.expirationDate = this.details.expirationDate;
+        }
+        return data;
+    }
+
+    /**
+     * assert cookie params
+     * @param {CookieDetails} details
+     */
+    static assertParams (details) {
+        if (!details.url) {
+            throw new Error('details.url required');
+        }
+    }
+}
